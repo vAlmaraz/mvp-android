3d18fd12f25c698cb95b3724b16bad623112569b
Víctor Almaraz <support@valmaraz.com>
2016-04-17 13:42:32 +0200

    Wrappers for network image loaders and ui messages

---
+++ app/src/main/java/com/valmaraz/mvp/view/ImageLoader.java
@@ -0,0 +1,17 @@
+package com.valmaraz.mvp.view;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by Víctor Almaraz on 17/04/2016.
+ * http://www.valmaraz.com
+ */
+public class ImageLoader {
+
+    public static void loadImage(Context context, String url, ImageView iv) {
+        Picasso.with(context).load(url).into(iv);
+    }
+}
