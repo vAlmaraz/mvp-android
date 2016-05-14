938caffb1b051ae6751dabeb444b75e4a78958db
Víctor Almaraz <support@valmaraz.com>
2016-05-14 19:29:05 +0200

    Details screen showing max min temp. fixes #2

--- app/src/main/java/com/valmaraz/mvp/view/activity/BaseActivity.java
+++ app/src/main/java/com/valmaraz/mvp/view/activity/BaseActivity.java
@@ -3,6 +3,7 @@ package com.valmaraz.mvp.view.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.valmaraz.mvp.Navigator;
 
@@ -18,5 +19,16 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         navigator = new Navigator();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/valmaraz/mvp/view/activity/WeatherDetailsActivity.java
+++ app/src/main/java/com/valmaraz/mvp/view/activity/WeatherDetailsActivity.java
@@ -31,10 +31,14 @@ public class WeatherDetailsActivity extends BaseActivity implements WeatherDetai
     View container;
     @Bind(R.id.act_weatherdetails_iv_weather)
     ImageView ivWeather;
-    @Bind(R.id.act_weatherdetails_tv_city)
-    TextView tvCityName;
     @Bind(R.id.act_weatherdetails_tv_temp)
     TextView tvTemp;
+    @Bind(R.id.act_weatherdetails_tv_sky)
+    TextView tvSky;
+    @Bind(R.id.act_weatherdetails_tv_temp_max)
+    TextView tvTempMax;
+    @Bind(R.id.act_weatherdetails_tv_temp_min)
+    TextView tvTempMin;
     @Bind(R.id.act_weatherdetails_tv_pressure)
     TextView tvPressure;
     @Bind(R.id.act_weatherdetails_tv_humidity)
@@ -103,12 +107,14 @@ public class WeatherDetailsActivity extends BaseActivity implements WeatherDetai
             image = WeatherRepository.getIconUrlForId(city.weatherList.get(0).icon);
         }
         ImageLoader.loadImage(this, image, ivWeather);
-        tvCityName.setText(city.name);
         tvTemp.setText(getString(R.string.temperature_value, city.main.temp));
     }
 
     private void showCompleteData(City city) {
         showBasicData(city);
+        tvSky.setText(city.weatherList.get(0).main);
+        tvTempMax.setText(getString(R.string.temperature_value, city.main.temp_max));
+        tvTempMin.setText(getString(R.string.temperature_value, city.main.temp_min));
         tvPressure.setText(getString(R.string.pressure_value, city.main.pressure));
         tvHumidity.setText(getString(R.string.humidity_value, city.main.humidity));
         tvWind.setText(getString(R.string.wind_value, city.wind.speed));
